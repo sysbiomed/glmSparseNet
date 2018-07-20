0b3e65f2e7621d1f9474df170e659e6d60a2bcfd
André Veríssimo <afsverissimo@gmail.com>
2018-07-20 03:16:46 +0100

    adds draw kaplan test

---
+++ tests/testthat/test-draw.kaplan.R
@@ -0,0 +1,53 @@
+context("draw.kaplan")
+
+library(survival)
+
+test_that("Calculates kaplan-meier data", {
+  result <- draw.kaplan(c(age = 1), ovarian$age, data.frame(time = ovarian$futime, status = ovarian$fustat))
+
+  expect_lt(result$pvalue - 0.0518148, 1e-2)
+})
+
+test_that('Only one group in kaplan.meier gives error', {
+  expect_error(draw.kaplan(c(age = 0), ovarian$age, data.frame(time = ovarian$futime, status = ovarian$fustat)))
+})
+
+test_that('All combinations of parameters possible for draw.kapan', {
+  xdata <- ovarian[,c('age', 'resid.ds')]
+  ydata <- data.frame(time = ovarian$futime, status = ovarian$fustat)
+
+  # list, data.frame, data.frame
+  expect_silent(draw.kaplan(list(c(1,0)), xdata, ydata))
+  # list, matrix, data.frame
+  expect_silent(draw.kaplan(list(c(1,0)), as.matrix(xdata), ydata))
+
+  # list, numeric, data.frame
+  expect_silent(draw.kaplan(list(c(1)), xdata$age, ydata))
+  # list, numeric, data.frame
+  expect_silent(draw.kaplan(1, xdata$age, ydata))
+
+  # numeric, data.frame, data.frame
+  expect_silent(draw.kaplan(c(1,0), xdata, ydata))
+  # numeric, matrix, data.frame
+  expect_silent(draw.kaplan(c(1,0), as.matrix(xdata), ydata))
+})
+
+test_that('Some bad arguments for draw.kaplan', {
+  xdata <- ovarian[,c('age', 'resid.ds')]
+  ydata <- data.frame(time = ovarian$futime, status = ovarian$fustat)
+
+  # list, data.frame, data.frame
+  expect_error(draw.kaplan(list(c(1,0), c(0,1,2)), xdata, ydata))
+  # list, matrix, data.frame
+  expect_error(draw.kaplan(list(c(1,0)), as.matrix(xdata), ydata[1:10,]))
+
+  # list, numeric, data.frame
+  expect_error(draw.kaplan(list(c(1,0)), xdata$age, ydata))
+  # list, numeric, data.frame
+  expect_error(draw.kaplan(c(1,2), xdata$age, ydata))
+
+  # numeric, data.frame, data.frame
+  expect_error(draw.kaplan(c(1,0,1), xdata, ydata))
+  # numeric, matrix, data.frame
+  expect_error(draw.kaplan(c(1,0), t(as.matrix(xdata)), ydata))
+})
