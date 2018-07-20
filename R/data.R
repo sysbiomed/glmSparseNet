fcb5eb9de056c86b6e1e8212cb9d41ffa3ed854c
André Veríssimo <afsverissimo@gmail.com>
2018-07-20 17:35:24 +0100

    adds all.interactions data

--- R/data.R
+++ R/data.R
@@ -29,3 +29,15 @@ NULL
 #' @references \url{https://portal.gdc.cancer.gov/}
 #' @keywords data
 NULL
+
+#' Cache of protein-protein interactions dataset, as it takes some time to retrieve and process
+#' this will facilitate the vignette building
+#'
+#' It was filtered with combined_scores and individual scores below 700 without text-based scores
+#'
+#' @name all.interactions.700
+#' @docType data
+#' @references \url{https://string-db.org/}
+#' @keywords data
+NULL
+
