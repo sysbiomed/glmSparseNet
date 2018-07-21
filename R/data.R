bae26ecf23aa80981dc9271215e4ac10b9799c7c
André Veríssimo <afsverissimo@gmail.com>
2018-07-21 13:39:28 +0100

    remove some datasets and better use of curatedTCGAdata

--- R/data.R
+++ R/data.R
@@ -6,30 +6,6 @@
 #' @keywords data
 NULL
 
-#' TCGA Breast cancer sample data set to use with survival model in vignettes
-#'
-#' @name sample.BRCA.survival
-#' @docType data
-#' @references \url{https://portal.gdc.cancer.gov/}
-#' @keywords data
-NULL
-
-#' TCGA Prostate cancer sample data set to use with survival model in vignettes
-#'
-#' @name sample.PRAD.survival
-#' @docType data
-#' @references \url{https://portal.gdc.cancer.gov/}
-#' @keywords data
-NULL
-
-#' TCGA Skin melanoma cancer sample data set to use with survival model in vignettes
-#'
-#' @name sample.SKCM.survival
-#' @docType data
-#' @references \url{https://portal.gdc.cancer.gov/}
-#' @keywords data
-NULL
-
 #' Cache of protein-protein interactions dataset, as it takes some time to retrieve and process
 #' this will facilitate the vignette building
 #'
