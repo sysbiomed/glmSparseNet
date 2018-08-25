8deea33a92c6516d545ebcfc3fea8357fca067a2
André Veríssimo <afsverissimo@gmail.com>
2018-08-25 01:56:21 +0000

    adds required imports from bioc automated feedback

--- R/imports_no_code.R
+++ R/imports_no_code.R
@@ -2,6 +2,10 @@
 #' @import Matrix
 #' @import glmnet
 #' @import ggfortify
+#' @import methods
+#' @import forcats
+#' @import sparsebn
+#' @import sparsebnUtils
 #' @import MultiAssayExperiment
 #' @import SummarizedExperiment
 NULL
