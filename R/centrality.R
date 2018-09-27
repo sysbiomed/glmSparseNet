8825ed0eec8f7773cdd5669b301d4d28be683827
André Veríssimo <afsverissimo@gmail.com>
2018-09-27 19:01:11 +0100

    convert to regular function degree.*

--- R/centrality.R
+++ R/centrality.R
@@ -142,31 +142,16 @@ networkGenericParallel <- function(fun, fun.prefix,
 #' degreeCor(xdata)
 #' degreeCor(xdata, cutoff = .5)
 #' degreeCor(xdata, cutoff = .5, consider.unweighted = TRUE)
-setGeneric('degreeCor', function(xdata, cutoff = 0, consider.unweighted = FALSE,
-                                 force.recalc.degree = FALSE, force.recalc.network = FALSE,
-                                 n.cores = 1, ...) {
-  stop('first argument must be a matrix')
-})
-
-#' Calculate the degree of the correlation network based on xdata
-#'
-#' @param xdata calculate correlation matrix on each column
-#' @param cutoff positive value that determines a cutoff value
-#' @param consider.unweighted consider all edges as 1 if they are greater than 0
-#' @param n.cores number of cores to be used
-#' @param force.recalc.degree force recalculation of penalty weights (but not the network), instead of going to cache
-#' @param force.recalc.network force recalculation of network and penalty weights, instead of going to cache
-#' @param ... extra parameters for cor function
-#'
-#' @return a vector of the degrees
-#' @export
-setMethod('degreeCor', signature('matrix'), function(xdata, cutoff = 0, consider.unweighted = FALSE,
-                                                     force.recalc.degree = FALSE, force.recalc.network = FALSE,
-                                                     n.cores = 1, ...) {
+degreeCor <- function(xdata, cutoff = 0, consider.unweighted = FALSE,
+                      force.recalc.degree = FALSE, force.recalc.network = FALSE,
+                      n.cores = 1, ...) {
+  if (!is(xdata, 'matrix')) {
+    stop('xdata argument must be a matrix object')
+  }
   return(degreeGeneric(stats::cor, 'correlation', xdata, cutoff = cutoff, consider.unweighted = consider.unweighted,
                        force.recalc.degree = force.recalc.degree, force.recalc.network = force.recalc.network,
                        n.cores = n.cores, ...))
-})
+}
 
 #' Calculate the degree of the covariance network based on xdata
 #'
@@ -187,31 +172,18 @@ setMethod('degreeCor', signature('matrix'), function(xdata, cutoff = 0, consider
 #' degreeCov(xdata)
 #' degreeCov(xdata, cutoff = .5)
 #' degreeCov(xdata, cutoff = .5, consider.unweighted = TRUE)
-setGeneric('degreeCov', function(xdata, cutoff = 0, consider.unweighted = FALSE,
-                                 force.recalc.degree = FALSE, force.recalc.network = FALSE,
-                                 n.cores = 1, ...) {
-  stop('first argument must be a matrix')
-})
+degreeCov <- function(xdata, cutoff = 0, consider.unweighted = FALSE,
+                      force.recalc.degree = FALSE, force.recalc.network = FALSE,
+                      n.cores = 1, ...) {
 
-#' Calculate the degree of the covariance network based on xdata
-#'
-#' @param xdata calculate correlation matrix on each column
-#' @param cutoff positive value that determines a cutoff value
-#' @param consider.unweighted consider all edges as 1 if they are greater than 0
-#' @param n.cores number of cores to be used
-#' @param force.recalc.degree force recalculation of penalty weights (but not the network), instead of going to cache
-#' @param force.recalc.network force recalculation of network and penalty weights, instead of going to cache
-#' @param ... extra parameters for cov function
-#'
-#' @return a vector of the degrees
-#' @export
-setMethod('degreeCov', signature('matrix'), function(xdata, cutoff = 0, consider.unweighted = FALSE,
-                                                     force.recalc.degree = FALSE, force.recalc.network = FALSE,
-                                                     n.cores = 1, ...) {
+  if (!is(xdata, 'matrix')) {
+    stop('xdata argument must be a matrix object')
+  }
+
   return(degreeGeneric(stats::cov, 'correlation', xdata, cutoff = cutoff, consider.unweighted = consider.unweighted,
                        force.recalc.degree = force.recalc.degree, force.recalc.network = force.recalc.network,
                        n.cores = n.cores, ...))
-})
+}
 
 
 
