1086ae98990e401b90d5511cabb3b90fb5b5f5b5
André Veríssimo <afsverissimo@gmail.com>
2018-09-30 02:51:36 +0100

    speeds up vignette building

--- R/data.R
+++ R/data.R
@@ -1,15 +1,14 @@
-#' Cache of protein-protein interactions dataset, as it takes some time to
-#' retrieve and process
-#' this will facilitate the vignette building
+#' Cache of protein-protein network, as it takes some time to
+#' retrieve and process this will facilitate the vignette building
 #'
 #' It was filtered with combined_scores and individual scores below 700 without
 #' text-based scores
 #'
-#' @name string.interactions.700.cache
+#' @name string.network.700.cache
 #' @docType data
 #' @references \url{https://string-db.org/}
 #' @keywords data
 #' @examples
-#' head(string.interactions.700.cache)
-'string.interactions.700.cache'
+#' head(string.network.700.cache)
+'string.network.700.cache'
 
