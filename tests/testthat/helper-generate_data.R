a7405a66c55cfb6f8f398632b4c7dfa5457ee674
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-02-10 01:51:05 +0100

    more minor tests 68%

--- tests/testthat/helper-generate_data.R
+++ tests/testthat/helper-generate_data.R
@@ -1,5 +1,40 @@
 random_xdata <- function(n_values = 30000, n_row = 500, seed = 1985) {
   set.seed(seed)
   xdata <- matrix(rnorm(n_values), nrow = n_row)
+}
 
+prepare_mae <- function() {
+  tmp_env <- new.env()
+  data("miniACC", package = "MultiAssayExperiment", envir = tmp_env)
+
+  xdata <- tmp_env$miniACC
+
+  event_ix <- which(!is.na(
+    MultiAssayExperiment::colData(xdata)$days_to_death
+  ))
+  cens_ix <- which(!is.na(
+    MultiAssayExperiment::colData(xdata)$days_to_last_followup
+  ))
+
+  xdata$surv_event_time <- array(
+    NA_integer_, nrow(MultiAssayExperiment::colData(xdata))
+  )
+  xdata$surv_event_time[event_ix] <- xdata$days_to_death[event_ix]
+  xdata$surv_event_time[cens_ix] <- xdata$days_to_last_followup[cens_ix]
+
+  # Keep only valid individuals
+  valid_ix <- as.vector(
+    !is.na(xdata$surv_event_time) &
+      !is.na(xdata$vital_status) &
+      xdata$surv_event_time > 0
+  )
+  xdata_valid <- xdata[
+    , rownames(MultiAssayExperiment::colData(xdata))[valid_ix]
+  ]
+  ydata_valid <- MultiAssayExperiment::colData(
+    xdata_valid
+  )[, c("surv_event_time", "vital_status")]
+  colnames(ydata_valid) <- c("time", "status")
+
+  list(xdata = xdata_valid, ydata = ydata_valid)
 }
--- tests/testthat/test-deprecation-utils.R
+++ tests/testthat/test-deprecation-utils.R
@@ -4,3 +4,50 @@ test_that("Deprecated functions", {
   my.symbols() |> lifecycle::expect_deprecated()
   hallmarks() |> lifecycle::expect_defunct()
 })
+
+test_that("glmOrphan: deprecated parameters", {
+  data <- prepare_mae()
+
+  glm_args <- list(
+    data$xdata,
+    data$ydata,
+    "correlation",
+    family = "cox",
+    network.options = networkOptions(minDegree = .2),
+    experiment.name = "RNASeq2GeneNorm"
+  )
+
+  list(glmOrphan, glmHub, glmDegree) |>
+    lapply(function(fun_name) {
+      do.call(fun_name, glm_args) |>
+        coef() |>
+        nrow() |>
+        expect_equal(data$xdata[["RNASeq2GeneNorm"]] |> nrow()) |>
+        expect_warning("experiments.* dropped") |>
+        expect_message("harmonizing input")
+    })
+})
+
+test_that("cv.glmOrphan: deprecated parameters", {
+  data <- prepare_mae()
+
+  glm_args <- list(
+    data$xdata,
+    data$ydata,
+    "correlation",
+    family = "cox",
+    nfolds = 5,
+    network.options = networkOptions(minDegree = .2),
+    experiment.name = "RNASeq2GeneNorm"
+  )
+
+  list(cv.glmOrphan, cv.glmHub, cv.glmDegree) |>
+    lapply(function(fun_name) {
+      do.call(fun_name, glm_args) |>
+        coef() |>
+        nrow() |>
+        expect_equal(data$xdata[["RNASeq2GeneNorm"]] |> nrow()) |>
+        expect_warning("experiments.* dropped") |>
+        expect_message("harmonizing input")
+    })
+})
